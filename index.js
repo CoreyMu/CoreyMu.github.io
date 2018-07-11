769eb565e68303672518284cac9de781cdd6320c
corey2915 <32797309+corey2915@users.noreply.github.com>
2018-07-11 17:58:08 -0400

    fix bugs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log("index.js is loading...")
+
 var screens = new Map();
 
 loadData();
@@ -9,85 +11,94 @@ function loadData() {
     .then(function(jsonObj) {
       database = jsonObj;
       console.log("Database Loaded Successfully");
-    }).then(setup)
+    }).then(function () {
+      hi(database)
+    })
   });
 }
 
-setupClick("step-up","welcomescreen");
+setupClick("step-up", "welcomescreen");
 
-setupClick("play","pickscreen");
+setupClick("play", "pickscreen");
 
-setupClick("contorl","contorlscreen");
+setupClick("contorl", "contorlscreen");
 
-setupClick("keyborad","keyboradscreen");
+setupClick("keyborad", "keyboradscreen");
 
-setupClick("next","contorlscreen1");
+setupClick("next", "contorlscreen1");
 
-setupClick("next1","contorlscreen2");
+setupClick("next1", "contorlscreen2");
 
-setupClick("next2","contorlscreen3");
+setupClick("next2", "contorlscreen3");
 
-setupClick("knext","keyboradscreen1");
+setupClick("knext", "keyboradscreen1");
 
-setupClick("knext1","keyboradscreen2");
+setupClick("knext1", "keyboradscreen2");
 
-setupClick("knext2","keyboradscreen3");
+setupClick("knext2", "keyboradscreen3");
 
-setupClick("skip","songscreen");
+setupClick("skip", "songscreen");
 
-setupClick("kskip","songscreen");
+setupClick("kskip", "songscreen");
 
-setupClick("wskip","welcomescreen");
+setupClick("wskip", "welcomescreen");
 
-setupClick("go","songscreen");
+setupClick("go", "songscreen");
 
-setupClick("kgo","songscreen");
+setupClick("kgo", "songscreen");
 
-setupClick("wgo","welcomescreen");
+setupClick("wgo", "welcomescreen");
 
-setupClick("settings","settingscreen");
+setupClick("settings", "settingscreen");
 
-setupClick("Howtousethewebsite","screenscreen");
+setupClick("Howtousethewebsite", "screenscreen");
 
-setupClick("Howtousethecontorl","contorlscreen");
+setupClick("Howtousethecontorl", "contorlscreen");
 
-setupClick("Howtousethekeyborad","keyboradscreen");
+setupClick("Howtousethekeyborad", "keyboradscreen");
 
-setupClick("wnext","screenscreen1");
+setupClick("wnext", "screenscreen1");
 
-setupClick("wnext1","screenscreen2");
+setupClick("wnext1", "screenscreen2");
 
-setupClick("wnext2","screenscreen3");
+setupClick("wnext2", "screenscreen3");
 
-$(window).on('hashchange', function(){
-        render(decodeURI(window.location.hash));
-    });
+$(window).on('hashchange', function() {
+    render(decodeURI(window.location.hash));
+});
+
 function render(url) {
-  console.log(url)
-  function logMapElements(value, key, map) {
-    console.log(value);
-    if (url=="#"+key){
-      value.style.display = "block";
-    }else {
-      value.style.display = "none";
+    console.log(url)
+
+    function logMapElements(value, key, map) {
+        console.log(value);
+        if (url == "#" + key) {
+            value.style.display = "block";
+        } else {
+            value.style.display = "none";
+        }
     }
-  }
-  screens.forEach(logMapElements);
-  console.log(screens);
-    }
+    screens.forEach(logMapElements);
+    console.log(screens);
+}
+
 function go(hash) {
-    window.location = "index.html#"+hash;
+    window.location = "index.html#" + hash;
 }
-function setupClick(div,hashstring) {
-  var button=document.getElementById(div);
-  button.addEventListener("click",function(){
-  go(hashstring);
-  });
-  addScreen(hashstring);
+
+function setupClick(div, hashstring) {
+    var button = document.getElementById(div);
+    button.addEventListener("click", function() {
+        go(hashstring);
+    });
+    addScreen(hashstring);
 }
+
 function addScreen(Screen) {
-var ScreenElement=document.getElementById(Screen);
-screens.set(Screen,ScreenElement);
+    var ScreenElement = document.getElementById(Screen);
+    screens.set(Screen, ScreenElement);
 
 }
-      go("welcomescreen");
+go("welcomescreen");
+
+console.log("index.js is alive")
---
+++ songoption.js
@@ -0,0 +1,28 @@
+console.log("songoption.js is loading...");
+function hi(database) {
+  console.log(database)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("songoption.js is alive");
