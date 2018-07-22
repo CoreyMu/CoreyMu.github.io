29859e1a4243f5d721d8a155dd5e9d056012b87b
corey2915 <32797309+corey2915@users.noreply.github.com>
2018-07-22 11:38:11 -0400

    add some picture and also add bpm in the json becase of the game

--- dancearrow.js
+++ dancearrow.js
@@ -1,33 +1,17 @@
 console.log("dancearrow.js is loading...");
+loadData();
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function loadData() {
+    fetch("database.json")
+        .then(function(response) {
+            response.json()
+                .then(function(jsonObj) {
+                    database = jsonObj;
+                    console.log("Database Loaded Successfully");
+                })
+        });
+}
 console.log("dancearrow.js is alive...");
