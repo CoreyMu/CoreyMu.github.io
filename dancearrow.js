4e3e084fb13e9042622b90ffbd8a154ff0cb7421
corey2915 <32797309+corey2915@users.noreply.github.com>
2018-07-23 15:31:12 -0400

    fix mucis

--- dancearrow.js
+++ dancearrow.js
@@ -1,17 +1,3 @@
 console.log("dancearrow.js is loading...");
-loadData();
 
-
-
-
-function loadData() {
-    fetch("database.json")
-        .then(function(response) {
-            response.json()
-                .then(function(jsonObj) {
-                    database = jsonObj;
-                    console.log("Database Loaded Successfully");
-                })
-        });
-}
 console.log("dancearrow.js is alive...");
--- songoption.js
+++ songoption.js
@@ -120,37 +120,37 @@ function updatePlayback(song) {
 
 // l
 
-function updateSeekSlider() {
-    // set the seekSlider’s value attribute to nowPlayingAudio’s currentTime attribute
-    seekSlider.value = nowPlayingAudio.currentTime;
-    // set the seekSlider’s max attribute to nowPlayingAudio’s duration attribute
-    seekSlider.max = nowPlayingAudio.duration;
-    // Create a variable called "seconds", set it to nowPlayingAudio's currentTim
-    if (nowPlayingAudio.currentTime >= nowPlayingAudio.duration) {
-        if (repeatState === 0) {
-            // Set seekSlider’s value to 0
-            seekSlider.value = 0;
-            // Set currentTimeDisplay's inner HTML text to "0:00"
-            currentTimeDisplay.innerHTML = "0:00";
-            // Pause nowPlayingAudio
-            nowPlayingAudio = "pause";
-            // Set playButton's inner HTML text to "play_arrow"
-            playButton.innerHTML = "play_arrow";
-        } else if (repeatState === 1) {
-            // play next track
-
-            // Call playNextSong()
-            playNextSong();
-        } else {
-            // repeat current track
-
-            // Set seekSlider's value to 0
-            seekSlider.value = 0;
-            // Load nowPlayingAudio
-            nowPlayingAudio.load();
-            // Play nowPlayingAudio
-            nowPlayingAudio.play();
-        }
-    }
-}
+// function updateSeekSlider() {
+//     // set the seekSlider’s value attribute to nowPlayingAudio’s currentTime attribute
+//     seekSlider.value = nowPlayingAudio.currentTime;
+//     // set the seekSlider’s max attribute to nowPlayingAudio’s duration attribute
+//     seekSlider.max = nowPlayingAudio.duration;
+//     // Create a variable called "seconds", set it to nowPlayingAudio's currentTim
+//     if (nowPlayingAudio.currentTime >= nowPlayingAudio.duration) {
+//         if (repeatState === 0) {
+//             // Set seekSlider’s value to 0
+//             seekSlider.value = 0;
+//             // Set currentTimeDisplay's inner HTML text to "0:00"
+//             currentTimeDisplay.innerHTML = "0:00";
+//             // Pause nowPlayingAudio
+//             nowPlayingAudio = "pause";
+//             // Set playButton's inner HTML text to "play_arrow"
+//             playButton.innerHTML = "play_arrow";
+//         } else if (repeatState === 1) {
+//             // play next track
+//
+//             // Call playNextSong()
+//             playNextSong();
+//         } else {
+//             // repeat current track
+//
+//             // Set seekSlider's value to 0
+//             seekSlider.value = 0;
+//             // Load nowPlayingAudio
+//             nowPlayingAudio.load();
+//             // Play nowPlayingAudio
+//             nowPlayingAudio.play();
+//         }
+//     }
+// }
 console.log("songoption.js is alive");
